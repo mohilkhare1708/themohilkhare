4a722773e0dc23d8a47ca7a7932cb026f8ffe466
mohilkhare1708 <mohilkhare17@gmail.com>
2021-07-05 23:20:34 +0530

    dynamic component lezzgo

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Banner from './components/Banner';
 import Scrolldown from './components/Scrolldown';
 import Contact from './components/Contact';
 import DownloadResume from './components/DownloadResume';
+import ExperienceItem from './components/ExperienceItem';
 
 function App() {
   return (
@@ -22,55 +23,38 @@ function App() {
       <section id="about">
         <div class="row">
           <div class="three columns">
-              <img class="profile-pic"  src="images/profilepic.jpg" alt="" />
+            <img class="profile-pic"  src="images/profilepic.jpg" alt="" />
           </div>
           <div class="nine columns main-col">
-              <h2>About Me</h2>
-              <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
-              </p>
-              <div class="row">
-                <Contact />
-                <DownloadResume />
-              </div>
-
+            <h2>About Me</h2>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+              voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
+            </p>
+            <div class="row">
+              <Contact />
+              <DownloadResume />
+            </div>
           </div>
-
         </div>
 
-        </section>
-        <section id="resume">
+      </section>
+      <section id="resume">
         <div class="row education">
-
           <div class="three columns header-col">
-              <h1><span>Education</span></h1>
+            <h1><span>Education</span></h1>
           </div>
-
           <div class="nine columns main-col">
-
-              <div class="row item">
-
-                <div class="twelve columns">
-
-                    <h3>University of Life</h3>
-                    <p class="info">Master in Graphic Design <span>&bull;</span> <em class="date">April 2007</em></p>
-
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-                    ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
-                    </p>
-
-                </div>
-
-              </div> 
-
+              <ExperienceItem 
+                orguni = 'Hey'
+                courserole = 'heyy'
+                date = 'yoo'
+                desc = 'yikes'
+              />
               <div class="row item">
 
                 <div class="twelve columns">
