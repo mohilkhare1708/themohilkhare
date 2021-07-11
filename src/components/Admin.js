1730c6a6850ebe88c64074f03463221d222071d8
mohilkhare1708 <mohilkhare17@gmail.com>
2021-07-11 16:51:32 +0530

    one field addition

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -6,10 +6,40 @@ function Admin() {
     <div class="row banner">
       <form>
         <h4>Add Education / Work Experience</h4>
-        <input class="name" type="text" id="orguni" name="orguni" placeholder="Organization / University" />
-        <input class="name" type="text" id="title" name="title" placeholder="Course / Job Title" />
-        <input class="name" type="text" id="date" name="date" placeholder="Date" />
-        <input class="name" type="text" id="desc" name="desc" placeholder="Description" />
+        <label for="type">Choose type</label>
+        <select name="type" id="type">
+          <option value="education">Education</option>
+          <option value="workex">Work Experience</option>
+        </select>
+        <br /><br />
+        <input
+          class="name"
+          type="text"
+          id="orguni"
+          name="orguni"
+          placeholder="Organization / University"
+        />
+        <input
+          class="name"
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Course / Job Title"
+        />
+        <input
+          class="name"
+          type="text"
+          id="date"
+          name="date"
+          placeholder="Date"
+        />
+        <input
+          class="name"
+          type="text"
+          id="desc"
+          name="desc"
+          placeholder="Description"
+        />
         <input class="sub" type="submit" value="Add This!" />
       </form>
     </div>
