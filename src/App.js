ae0992eb185faabe5a8a6138b726d54d1c42fbb8
Mohil Khare <mohilkhare17@gmail.com>
2021-11-23 19:57:45 +0530

    mail thingy and title tag dine

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/css/App.css";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
+import Helmet from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mohil Khare</title>
+      </Helmet>
       <Router>
         <Switch>
           <Route path="/" exact component={() => <Home />} />
--- src/components/DownloadResume.js
+++ src/components/DownloadResume.js
@@ -3,12 +3,24 @@ import mohil_khare_resume from "../assets/Mohil_Khare_Resume.pdf";
 
 function DownloadResume() {
   return (
-    <div class="columns download">
-      <p>
-        <a href={mohil_khare_resume} class="button">
-          <i class="fa fa-download"></i>Download Resume
-        </a>
-      </p>
+    <div>
+      <div class="columns download">
+        <p>
+          <a href={mohil_khare_resume} class="button">
+            <i class="fa fa-download"></i>Download Resume
+          </a>
+        </p>
+      </div>
+      <div class="columns download">
+        <p>
+          <a
+            href="mailto:mohilkhare17@gmail.com?subject=Hey Mohil!"
+            class="button"
+          >
+            <i class="fa fa-envelope"></i>Send me a mail!
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
